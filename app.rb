f82f55f9503483b5dcb18fbe66ce0cbe750a35c7
Moosan63 <moosan63@Pleiades.local>
2012-10-09 00:56:37 +0900

    その他とかを追加！

--- app.rb
+++ app.rb
@@ -9,12 +9,12 @@ include Magick
 #== sinatra session
 enable :sessions
 configure do
-    use Rack::Session::Cookie,
-    #:key => 'rack.session',
-    #:domain => 'foo.com',
-    #:path => '/',
-    :expire_after => 3600,
-    :secret => 'change'
+  use Rack::Session::Cookie,
+  #:key => 'rack.session',
+  #:domain => 'foo.com',
+  #:path => '/',
+  :expire_after => 3600,
+  :secret => 'change'
 end
 ####################
 #   sinatraな部分  #
@@ -29,51 +29,51 @@ end
 
 post '/create' do
 
-    #csrf対策
-    if (params["token"] == session[:token]) && params[:file]
-        #アップロードされたファイルをまず保存
-        begin
-            save_file = STORE_DIR+File.extname(params[:file][:filename])
-            File.open(save_file, 'wb'){ |f| f.write(params[:file][:tempfile].read) }
-        rescue
-            puts "file write error"
-            return 
-        end
-        #合成
-        @image = composite(save_file, params["token"])
-        @msg = ""
-    else
-        @msg = "select your image file!"
-        @image = ""
+  #csrf対策
+  if (params["token"] == session[:token]) && params[:file]
+    #アップロードされたファイルをまず保存
+    begin
+      save_file = STORE_DIR+File.extname(params[:file][:filename])
+      File.open(save_file, 'wb'){ |f| f.write(params[:file][:tempfile].read) }
+    rescue
+      puts "file write error"
+      return 
     end
-    erb :create
+    #合成
+    @image = composite(save_file, params["token"])
+    @msg = ""
+  else
+    @msg = "select your image file!"
+    @image = ""
+  end
+  erb :create
 end
 
 #画像を2枚重ね合わせるメソッド
 private
 def composite(src_file_name, result_file_name)
-    begin
-        resultFileName = STORE_DIR + result_file_name + ".png"
-        
-        result = Image.from_blob(File.read(STORE_DIR+"../kazoo_origin_bg.png")).shift.resize(256,256)
-        img = Image.from_blob(File.read(src_file_name)).shift.resize(256,256)
-        result = result.composite(img, 0, 0, OverCompositeOp)
-        
-        result.write(resultFileName)
-        return "images/store/"+result_file_name+".png"
-    rescue
-        puts "file composite error"
-        return ""
-    end
+  begin
+    resultFileName = STORE_DIR + result_file_name + ".png"
+    
+    result = Image.from_blob(File.read(STORE_DIR+"../kazoo_origin_bg.png")).shift.resize(256,256)
+    img = Image.from_blob(File.read(src_file_name)).shift.resize(256,256)
+    result = result.composite(img, 0, 0, OverCompositeOp)
+    
+    result.write(resultFileName)
+    return "images/store/"+result_file_name+".png"
+  rescue
+    puts "file composite error"
+    return ""
+  end
 end
 
 #連続投稿を防ぐためのトークンを作成する
 private
 def create_token
-    token =''
-    a =->(){  [('a'..'z'),('A'..'Z'),('0'..'9')].map{|e| e.to_a }.flatten.sample }
-    (1..16).each do
-        token += a.()
-    end
-    return token
+  token =''
+  a =->(){  [('a'..'z'),('A'..'Z'),('0'..'9')].map{|e| e.to_a }.flatten.sample }
+  (1..16).each do
+    token += a.()
+  end
+  return token
 end
